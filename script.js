c034a4ecbbd08ea0410cee21fe4d9790929ac1c0
mahesh <122031410+mbhat8@users.noreply.github.com>
2023-11-06 15:59:23 +0530

    intemediate

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hi')
+//Declare an empty array for Library
+let myLibrary=[];
+
+//Object constructor for book object
+function Book(Title,Author,Pages,Read){
+    this.Title=Title;
+    this.Author=Author;
+    this.Pages=Pages;
+    this.Read=Read;
+}
+
+//Function for adding new book to Library
+function addBooktoLibrary(Title,Author,Pages,Read){
+    let book=new Book(Title,Author,Pages,Read);
+    myLibrary.push(book);
+}
